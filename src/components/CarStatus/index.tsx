3da2f4a69906cb9ea7bd51e1fb77f502d1041939
diego64 <diegoferreira1964@gmail.com>
2024-02-24 01:07:00 -0300

    Configuracao do bando de dados local

--- src/components/CarStatus/index.tsx
+++ src/components/CarStatus/index.tsx
@@ -8,7 +8,7 @@ type Props = TouchableOpacityProps & {
 }
 
 export function CarStatus({ licensePlate = null, ...rest }: Props) {
-  const Icon = licensePlate ? Key : Car
+  const Icon = licensePlate ? Car : Key;
   const message = licensePlate ? `Veículo ${licensePlate} em uso. ` : 'Nenhum veículo em uso. '
   const status = licensePlate ? 'chegada' : 'saída';
 
@@ -17,8 +17,8 @@ export function CarStatus({ licensePlate = null, ...rest }: Props) {
   return (
     <Container {...rest}>
       <IconBox>
-        <Icon 
-          size={32}
+        <Icon
+          size={52}
           color={theme.COLORS.BRAND_LIGHT}
         />
       </IconBox>
